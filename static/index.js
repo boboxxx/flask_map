f9f9aaa507dcbd1503dc6a59fe90064ca3a3d22a
bobo <1320124900@qq.com>
2024-03-24 23:12:34 +0000

    index

--- static/index.js
+++ static/index.js
@@ -4,19 +4,19 @@ var marker = L.marker([39.916611, 116.390748]).addTo(map);
 var car = L.polyline([], {color: 'red'}).addTo(map);
 
 setInterval(function() {
-    fetch('/gps-data')
+    fetch('/car1')
         .then(response => response.json())
         .then(data => {
-            var latlng = [msg.latitude, msg.longitude];
+            // var latlng = [msg.latitude, msg.longitude];
 
-            map.removeLayer(marker);
+            // map.removeLayer(marker);
 
-            marker = L.marker(latlng).addTo(map);
+            // marker = L.marker(latlng).addTo(map);
 
-            car.addLatLng(latlng);
+            // car.addLatLng(latlng);
             // 在这里处理GPS数据，例如更新地图标记
             console.log('Latitude:', data.latitude);
             console.log('Longitude:', data.longitude);
         });
-}, 1000); 
+}, 2000); 
 
