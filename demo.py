b05e97884dc71498197234ed31c1e2702ff9651f
bobo <1320124900@qq.com>
2024-03-24 23:16:24 +0000

    demo

--- demo.py
+++ demo.py
@@ -14,7 +14,7 @@ def receive_data():
     data = request.get_json()
     print(data)
    
-    return jsonify({data: 'success'}), 200
+    return jsonify({'status': 'success'}), 200
 
 if __name__ == '__main__':
     app.run(debug=True, host = '0.0.0.0', port=5000)
