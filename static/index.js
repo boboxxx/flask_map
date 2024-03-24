b5e7f63cc431fdd849f58a8918b7f7476ca5ae2c
bobo <1320124900@qq.com>
2024-03-24 14:58:02 +0000

    index

--- static/index.js
+++ static/index.js
@@ -1,7 +1,19 @@
+var map = L.map('map').setView([39.916611, 116.390748], 13);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+var marker = L.marker([39.916611, 116.390748]).addTo(map);
+var car = L.polyline([], {color: 'red'}).addTo(map);
+
 setInterval(function() {
     fetch('/gps-data')
         .then(response => response.json())
         .then(data => {
+            var latlng = [msg.latitude, msg.longitude];
+
+            map.removeLayer(marker);
+
+            marker = L.marker(latlng).addTo(map);
+
+            car.addLatLng(latlng);
             // 在这里处理GPS数据，例如更新地图标记
             console.log('Latitude:', data.latitude);
             console.log('Longitude:', data.longitude);
