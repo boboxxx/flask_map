af700bf77f7fb3fe6ae5c6baa1510a647c49ce8f
bobo <1320124900@qq.com>
2024-03-26 20:54:55 +0000

    update app

--- app.py
+++ app.py
@@ -1,49 +1,36 @@
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 
-# 全局变量，用于存储 POST 请求中的数据
-car1 = None
-car2 = None
 # 设置跨域
 CORS(app, supports_credentials=True)
 
-@app.route('/data', methods=['POST', 'GET'])
+# 路由，接收从嵌入式设备发送过来的数据
+@app.route('/data', methods=['POST'])
 def receive_data():
-    global car1, car2
-    if request.method == 'POST':
-        # 获取 POST 请求中的数据
-        data = request.json
-        print('Received data:', data)
+ 
+    data = request.get_json()
 
-        # 检查数据中是否包含 'car1' 或 'car2' 键
-        if 'car1' in data:
-            car1 = data['car1']
-            print('car1:', car1)
-        elif 'car2' in data:
-            car2 = data['car2']
-            print('car2:', car2)
-        else:
-            return jsonify({"error": "Invalid data format"}), 400
-        
-        # 返回成功响应
-        return jsonify({"message": "Data received successfully"})
-    elif request.method == 'GET':
-        # 如果是 GET 请求，返回相应的车辆数据
-            if car1 and car2:
-                return jsonify({"car1": car1, "car2": car2})
-            elif car1:
-                return jsonify({"car1": car1})
-            elif car2:
-                return jsonify({"car2": car2})
-            else:
-                return jsonify({"error": "No data available"}), 400
-        
+    # 将数据通过 WebSocket 发送给 HTML 页面
+    socketio.emit('device_data', data)
+    return 'Data received and sent to clients'
 
+# HTML 页面
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
+
+# WebSocket 事件处理器
+@socketio.on('connect')
+def handle_connect():
+    print('Client connected')
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    print('Client disconnected')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    socketio.run(app, host='0.0.0.0', port=8080, debug=True)
